d37d4b0bcb1e6c7516fac46111fac1d5fe445357
kishida <naokikishida@gmail.com>
2016-04-09 05:30:50 +0900

    First MVC

---
+++ src/main/java/kis/di/annotation/Path.java
@@ -0,0 +1,15 @@
+package kis.di.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author naoki
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface Path {
+    String value();
+}
